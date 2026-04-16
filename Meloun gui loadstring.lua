54607abf089f2844921438bc8b002d3a866d1464
gruppjakob5-jpg <gruppjakob5@gmail.com>
2026-04-16 14:09:36 +0200

    Add Meloun GUI loadstring functionality

---
+++ Meloun gui loadstring.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gruppjakob5-jpg/Meloun-gui-v2---fe-bypass/3a5836a8da2b54646ec9ece670dee65114283433/Meloun%20gui%20FE%20bypass%20xester.Lua"))()
